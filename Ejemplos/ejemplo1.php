2270d2c8730377a7985a9946a0ea2b755370cea3
IsraelArbona <i.arbonammisena.edu.co>
2021-06-24 21:05:05 -0500

    Inicio de los proyectos

---
+++ Ejemplos/ejemplo1.php
@@ -0,0 +1,27 @@
+<?php
+
+    /*
+    echo "Adsi";
+    print('<br>');
+    print('Virtual');
+    */
+
+    // print("$minombre apellido");  comentar una linea codigo.
+
+    $programa = 'Adsi Virtual'
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+    <h1><?php echo 'lenguaje de php' ?> </h1>
+    <p><?php print($programa); ?> </p>
+</body>
+</html>
